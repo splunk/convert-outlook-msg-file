c18fc204463057329f1324f09cfb122fbee8bd6a
Steve Jackson <sjackson@splunk.com>
2025-10-03 13:14:24 -0800

    Enable surrogatepass flag for utf-16 encoding

--- outlookmsgfile.py
+++ outlookmsgfile.py
@@ -485,7 +485,7 @@ class UNICODE(VariableLengthValueLoader):
     # value is a bytestring. I haven't seen specified what character encoding
     # is used when the Unicode storage type is not used, so we'll assume it's
     # ASCII or Latin-1 like but we'll use UTF-8 to cover the bases.
-    return value.decode("utf16")
+    return value.decode("utf16", errors="surrogatepass")
 
 # TODO: The other variable-length tag types are "CLSID", "OBJECT".
 
